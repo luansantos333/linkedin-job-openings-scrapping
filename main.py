400679af1d640010520196d1b4a551c401f23dc5
luansantos333 <luangomespctu@gmail.com>
2026-01-18 20:42:50 -0300

    feat: scrape job lists

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def scrape_site():
     options.add_experimental_option('useAutomationExtension', False)
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
-    options.add_argument(r'user-data-dir=/tmp/.org.chromium.Chromium.scoped_dir.y1gjIB/profile_selenium')
+    options.add_argument(r'user-data-dir=' + os.getenv("PROFILE_DATA_DIR"))
     options.add_experimental_option("detach", True)
     service = Service(executable_path=os.getenv("CHROMEDRIVER_PATH"))
 
@@ -29,54 +29,77 @@ def scrape_site():
 
     web.get("https://www.glassdoor.com.br")
     wait = WebDriverWait(web, 10)
+    job_info = {}
+    all_jobs = []
     try:
-        google_login = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "colorCtaGoogle")))
-        google_login.click()
-        web.switch_to.window(web.window_handles[1])
-        if not web.find_element(By.CLASS_NAME, "pGzURd"):
-            web.find_element(By.XPATH, '//*[@id="identifierId"]').send_keys(os.getenv("GOOGLE_EMAIL"))
-            web.find_element(By.XPATH, '//*[@id="identifierNext"]/div/button/span').click()
-            password = wait.until(
-                EC.presence_of_element_located((By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input')))
-            password.send_keys(os.getenv("GOOGLE_PASSWORD"))
-            confirm = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="passwordNext"]/div/button/div[3]')))
-            actions = ActionChains(web)
-            actions.move_to_element(confirm).click().perform()
-        else:
-            web.find_element(By.CLASS_NAME, "yAlK0b").click()
-            wait._timeout = 5
-            if not wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="yDmH0d"]/c-wiz/main/div[3]/div/div/div[2]/div/div/button'))):
-                web.switch_to.window(web.window_handles[0])
-                wait._timeout = 10
-                login_btn = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="AuthPromptModalContainer"]/div/div/dialog/div[2]/div[2]/div/div/button')))
-                login_btn.click()
-                web.switch_to.window(web.window_handles[1])
-                form = web.find_element(By.ID, 'emailform')
-                form.find_element(By.TAG_NAME, 'button').click()
-                web.find_element(By.ID, 'googleContainer').click()
-            else:
+
+        if not web.find_element(By.CLASS_NAME, "ji8JI0ibjBNKYy6dBuvi"):
+
+            google_login = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "colorCtaGoogle")))
+            google_login.click()
+            web.switch_to.window(web.window_handles[1])
+            if not web.find_element(By.CLASS_NAME, "pGzURd"):
+                web.find_element(By.XPATH, '//*[@id="identifierId"]').send_keys(os.getenv("GOOGLE_EMAIL"))
+                web.find_element(By.XPATH, '//*[@id="identifierNext"]/div/button/span').click()
+                password = wait.until(
+                    EC.presence_of_element_located((By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input')))
+                password.send_keys(os.getenv("GOOGLE_PASSWORD"))
+                confirm = wait.until(
+                    EC.element_to_be_clickable((By.XPATH, '//*[@id="passwordNext"]/div/button/div[3]')))
                 actions = ActionChains(web)
-                confirm = wait.until(EC.element_to_be_clickable(
-                    (By.XPATH, '//*[@id="yDmH0d"]/c-wiz/main/div[3]/div/div/div[2]/div/div/button')))
                 actions.move_to_element(confirm).click().perform()
-                web.switch_to.window(web.window_handles[0])
-                login_btn = wait.until(EC.element_to_be_clickable(
-                    (By.XPATH, '//*[@id="AuthPromptModalContainer"]/div/div/dialog/div[2]/div[2]/div/div/button')))
-                login_btn.click()
-                web.switch_to.window(web.window_handles[1])
-                form = web.find_element(By.ID, 'emailform')
-                form.find_element(By.TAG_NAME, 'button').click()
-                web.find_element(By.ID, 'googleContainer').click()
+            else:
+                web.find_element(By.CLASS_NAME, "yAlK0b").click()
+                wait._timeout = 5
+                if not wait.until(EC.presence_of_element_located(
+                        (By.XPATH, '//*[@id="yDmH0d"]/c-wiz/main/div[3]/div/div/div[2]/div/div/button'))):
+                    web.switch_to.window(web.window_handles[0])
+                    wait._timeout = 10
+                    login_btn = wait.until(EC.element_to_be_clickable(
+                        (By.XPATH, '//*[@id="AuthPromptModalContainer"]/div/div/dialog/div[2]/div[2]/div/div/button')))
+                    login_btn.click()
+                    web.switch_to.window(web.window_handles[1])
+                    web.find_element(By.ID, 'onetrust-accept-btn-handler').click()
+                    form = web.find_element(By.ID, 'emailform')
+                    form.find_element(By.TAG_NAME, 'button').click()
+                    web.find_element(By.ID, 'googleContainer').click()
+                    wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'ji8JI0ibjBNKYy6dBuvi')))
+                else:
+                    actions = ActionChains(web)
+                    confirm = wait.until(EC.element_to_be_clickable(
+                        (By.XPATH, '//*[@id="yDmH0d"]/c-wiz/main/div[3]/div/div/div[2]/div/div/button')))
+                    actions.move_to_element(confirm).click().perform()
+                    web.switch_to.window(web.window_handles[0])
+                    login_btn = wait.until(EC.element_to_be_clickable(
+                        (By.XPATH, '//*[@id="AuthPromptModalContainer"]/div/div/dialog/div[2]/div[2]/div/div/button')))
+                    login_btn.click()
+                    web.switch_to.window(web.window_handles[1])
+                    web.find_element(By.ID, 'onetrust-accept-btn-handler').click()
+                    form = web.find_element(By.ID, 'emailform')
+                    form.find_element(By.TAG_NAME, 'button').click()
+                    web.find_element(By.ID, 'googleContainer').click()
+                    wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'ji8JI0ibjBNKYy6dBuvi')))
+        else:
+            count = 1000
+            for i in range(1, 5):
+                jobs = web.find_elements(By.CLASS_NAME, "JobCard_jobCardLeftContent__cHcGe")
+                for job in jobs:
+                    job_info.update({"vaga": job.find_elements(By.TAG_NAME, "a")[0].text,
+                                     "link": job.find_elements(By.TAG_NAME, "a")[1].get_attribute("href")})
+                    all_jobs.append(job_info)
 
-
-
-
+                web.execute_script(f"window.scrollTo(0,{count});")
+                time.sleep(2)
+                count = count * i
 
 
     except TimeoutException as e:
         print(e.msg)
-    # finally:
-    #     web.close()
+    finally:
+        print("--------------------------------\n\n")
+        all_jobs = all_jobs
+        print ("\n".join(all_jobs))
+        web.close()
 
 
 # Press the green button in the gutter to run the script.
